c43b176da0afd843ad7faf71309cefe1d19665c3
ANDRESOTELO <andresotelo2020@gmail.com>
2022-11-30 17:32:20 -0500

    ADDED | Dart

--- 0x01-dart-function/7-basketball-shots.dart
+++ 0x01-dart-function/7-basketball-shots.dart
@@ -1,27 +1,12 @@
-int whoWins(Map<String, int> teamA, Map<String, int> teamB) {
-  var t1 = 0;
-  var t2 = 0;
-
-  teamA.forEach((key, value) {
-    if (key == 'Free throws')
-      t1 += value;
-    else if (key == '2 pointer')
-      t1 += value * 2;
-    else if (key == '3 pointer') t1 += value * 3;
-  });
-
-  teamB.forEach((key, value) {
-    if (key == 'Free throws')
-      t2 += value;
-    else if (key == '2 pointer')
-      t2 += value * 2;
-    else if (key == '3 pointer') t2 += value * 3;
-  });
-
-  if (t1 > t2)
-    return 1;
-  else if (t1 == t2)
+int whoWins(Map<String, int> team1, Map<String, int> team2) {
+  int team1Score = (team1['Free throws'] ?? 0) +
+      ((team1['2 pointers'] ?? 0) * 2) +
+      ((team1['3 pointers'] ?? 0) * 2);
+  int team2Score = (team2['Free throws'] ?? 0) +
+      ((team2['2 pointers'] ?? 0) * 2) +
+      ((team2['3 pointers'] ?? 0) * 3);
+  if (team1Score == team2Score) {
     return 0;
-  else
-    return 2;
+  }
+  return team1Score > team2Score ? 1 : 2;
 }
