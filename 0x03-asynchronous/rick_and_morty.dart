b416e6c70ca6e4fa6f1d62e9595a5e17b7401c90
ANDRESOTELO <andresotelo2020@gmail.com>
2022-12-06 18:15:35 -0500

    ADDED | Dart

--- 0x03-asynchronous/rick_and_morty.dart
+++ 0x03-asynchronous/rick_and_morty.dart
@@ -1,7 +1,7 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 
-Future<void> printBbCharacters() async {
+Future<void> printRmCharacters() async {
   String url = 'https://rickandmortyapi.com/api/character';
   try {
     var response = await http.get(Uri.parse(url));
