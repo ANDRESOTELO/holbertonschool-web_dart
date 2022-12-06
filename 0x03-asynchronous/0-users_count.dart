3de962e7e68f0e2bf4cfeb4c0ab929c3db553891
ANDRESOTELO <andresotelo2020@gmail.com>
2022-12-06 18:19:28 -0500

    ADDED | Dart

--- 0x03-asynchronous/0-users_count.dart
+++ 0x03-asynchronous/0-users_count.dart
@@ -1,4 +1,8 @@
+// Wild that this is not a circular import
+import '0-main.dart';
+
 Future<void> usersCount() {
+  // Future<void> doesn't return anything
   return fetchUsersCount().then((count) {
     print(count);
   });
